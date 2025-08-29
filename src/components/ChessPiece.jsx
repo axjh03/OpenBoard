f67aa99fcc38df7186e8a676d020c2428f4a1867
Alok Jha <axjh03@gmail.com>
2025-08-29 12:06:05 -0500

    D

--- src/components/ChessPiece.jsx
+++ src/components/ChessPiece.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
 const ChessPiece = ({ type, color }) => {
-  // Chess piece icons from Flaticon
+  // Chess piece icons - using actual chess piece images
   const pieceIcons = {
     white: {
+      king: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/wK.svg",
+      queen: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/wQ.svg",
+      rook: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/wR.svg",
+      bishop: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/wB.svg",
+      knight: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/wN.svg",
+      pawn: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/wP.svg",
+    },
+    black: {
+      king: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/bK.svg",
+      queen: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/bQ.svg",
+      rook: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/bR.svg",
+      bishop: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/bB.svg",
+      knight: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/bN.svg",
+      pawn: "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/bP.svg",
+    },
+  };
+
+  // Fallback to Unicode symbols if images fail to load
+  const fallbackSymbols = {
+    white: {
       king: "♔",
-      queen: "♕", 
+      queen: "♕",
       rook: "♖",
       bishop: "♗",
       knight: "♘",
@@ -14,28 +34,8 @@ const ChessPiece = ({ type, color }) => {
     black: {
       king: "♚",
       queen: "♛",
-      rook: "♜", 
-      bishop: "♝",
-      knight: "♞",
-      pawn: "♟",
-    },
-  };
-
-  // Alternative: Use larger, more visible Unicode symbols
-  const pieceSymbols = {
-    white: {
-      king: "♔",
-      queen: "♕",
-      rook: "♖", 
-      bishop: "♗",
-      knight: "♘",
-      pawn: "♙",
-    },
-    black: {
-      king: "♚",
-      queen: "♛",
       rook: "♜",
-      bishop: "♝", 
+      bishop: "♝",
       knight: "♞",
       pawn: "♟",
     },
@@ -43,8 +43,17 @@ const ChessPiece = ({ type, color }) => {
 
   return (
     <div className="chess-piece">
-      <span className="piece-symbol">
-        {pieceSymbols[color][type]}
+      <img 
+        src={pieceIcons[color][type]} 
+        alt={`${color} ${type}`}
+        className="piece-symbol"
+        onError={(e) => {
+          e.target.style.display = 'none';
+          e.target.nextSibling.style.display = 'block';
+        }}
+      />
+      <span className="piece-symbol-fallback" style={{ display: 'none' }}>
+        {fallbackSymbols[color][type]}
       </span>
     </div>
   );
